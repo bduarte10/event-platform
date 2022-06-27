523f5bb775b7e6dd2df8f3182ab6a5920aed99ec
Bruno Duarte <duartebruno2208@gmail.com>
2022-06-26 21:43:17 -0300

    :sparkles: feat: inscrição via GraphQL

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4owqtp51bb001xs8v1l1qw8/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache(),
 });
